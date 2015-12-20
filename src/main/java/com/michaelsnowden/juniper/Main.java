f672ac02864a4d6a92a52573c01cc970efe37148
Michael Snowden <michael.snowden@glassdoor.com>
2015-12-20 12:53:31 -0800

    Allowing access from any origin

--- src/main/java/com/michaelsnowden/juniper/Main.java
+++ src/main/java/com/michaelsnowden/juniper/Main.java
@@ -16,6 +16,22 @@ public class Main {
         port(Integer.valueOf(System.getenv("PORT")));
         staticFileLocation("/public");
 
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+
         before(new Filter() {
             @Override
             public void handle(Request request, Response response) throws Exception {
